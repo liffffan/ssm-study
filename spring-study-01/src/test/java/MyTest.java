0334bd5ab9484dcada3def17c48fda2e8fa63ab9
Hunglish <354535209@qq.com>
2020-05-28 07:28:40 +0800

    初学 Spring

---
+++ spring-study-01/src/test/java/MyTest.java
@@ -0,0 +1,16 @@
+import com.kuang.pojo.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        // 获取 Spring 的上下文对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // 我们的对象都在 Spring 中的管理了，我们要使用，直接去里面取出来就可以了
+        Hello hello = (Hello) context.getBean("hello");
+        hello.show();
+
+
+    }
+}
