895c34031d8a01822714a6c94d574e1cbae95ddd
liffffan <liffffan@gmail.com>
2020-05-28 17:07:20 +0800

    spring-ioc

--- spring-study-01/src/test/java/MyTest.java
+++ spring-study-01/src/test/java/MyTest.java
@@ -8,9 +8,8 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         // 我们的对象都在 Spring 中的管理了，我们要使用，直接去里面取出来就可以了
+        // getBean 里的字符串对应 xml 里面 bean 的 id
         Hello hello = (Hello) context.getBean("hello");
         hello.show();
-
-
     }
 }
